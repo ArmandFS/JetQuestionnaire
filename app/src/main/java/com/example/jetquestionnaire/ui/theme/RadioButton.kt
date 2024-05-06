cafc20a0a74756af853f6d0de824d5c37e73460a
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-05-06 15:51:35 +0700

    Update RadioButton.kt with adding better spacing and padding

--- app/src/main/java/com/example/jetquestionnaire/ui/theme/RadioButton.kt
+++ app/src/main/java/com/example/jetquestionnaire/ui/theme/RadioButton.kt
@@ -5,13 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RadioButtonTest(){
@@ -22,11 +28,21 @@ fun RadioButtonTest(){
         verticalArrangement = Arrangement.Center
     ) {
         options.forEach { option ->
-            Row {
+            //modify the row
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .selectable(
+                    selected = selectedOption == option,
+                    onClick = { selectedOption = option})
+                .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                androidx.compose.material3.RadioButton(
                    selected = selectedOption == option,
-                   onClick = { selectedOption = option })
-                Text(text = option)
+                   onClick = null)
+                Text(text = option,
+                    modifier = Modifier.padding(start = 16.dp))
             }
         }
     }
