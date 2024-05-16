654dd1d613628c68d50974e6998b1383df9c16f8
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-05-16 22:07:01 +0700

    added lazycolumn to  main screen

--- app/src/main/java/com/example/jetquestionnaire/MainActivity.kt
+++ app/src/main/java/com/example/jetquestionnaire/MainActivity.kt
@@ -3,41 +3,19 @@ package com.example.jetquestionnaire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.state.ToggleableState
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.jetquestionnaire.ui.theme.EnterDate
 import com.example.jetquestionnaire.ui.theme.JetQuestionnaireTheme
 import com.example.jetquestionnaire.ui.theme.RadioButtonTest
 
@@ -51,10 +29,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                RadioButtonTest()
+                    //add scaffold
+                    Scaffold() {innerpadding ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Top
+                        ) {
+                            LazyColumnDemo()
+                        }
+                    }
+                    
                 }
             }
         }
+    }
+}
+
+
+
+//lazy column testing
+@Composable
+fun LazyColumnDemo() {
+    //question_list entar ambil dari api
+    val question_list = listOf(
+        "1", "2", "3", "4","5","6","7","8","9","10","11","12","13","14","15","16")
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        items(question_list) {item ->
+            RadioButtonTest()
+        }
+
     }
 }
 
