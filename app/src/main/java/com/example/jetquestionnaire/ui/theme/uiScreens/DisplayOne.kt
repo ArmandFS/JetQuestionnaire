085c4d22d48f56269e1a68fbe8b248ef2f8bfb51
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-06-02 17:30:26 +0700

    Update DisplayOne.kt

--- app/src/main/java/com/example/jetquestionnaire/ui/theme/uiScreens/DisplayOne.kt
+++ app/src/main/java/com/example/jetquestionnaire/ui/theme/uiScreens/DisplayOne.kt
@@ -3,25 +3,37 @@ package com.example.jetquestionnaire.uiScreens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
-//kotlin file for button to press before pop-up
 @Preview
 @Composable
-fun DisplayOne(){
-    Surface(modifier = Modifier.fillMaxSize()) {
+fun DisplayOne() {
+    var showDialog by remember { mutableStateOf(false) }
 
+    if (showDialog) {
+        AlertDialog(onDismiss = { showDialog = false })
+    }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-
-            Button(onClick = { /*TODO*/ }) {
+            Button(
+                onClick = { showDialog = true },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF5799FC))
+            ) {
                 Text(text = "Apa itu Diabetes Self Management Questionnaire?")
             }
         }
