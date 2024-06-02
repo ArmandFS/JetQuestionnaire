45456f5d8611cb5488c0ffc1aefed6d0aa37498c
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-06-02 22:47:58 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/jetquestionnaire/MainActivity.kt
+++ app/src/main/java/com/example/jetquestionnaire/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.jetquestionnaire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.jetquestionnaire.ui.theme.Divider1
 import com.example.jetquestionnaire.ui.theme.EnterDate
 import com.example.jetquestionnaire.ui.theme.JetQuestionnaireTheme
 import com.example.jetquestionnaire.ui.theme.RadioButtonTest
+import com.example.jetquestionnaire.uiScreens.DisplayOne
+import com.example.jetquestionnaire.uiScreens.MainHeader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,21 +31,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //add scaffold
-                    Scaffold() {innerpadding ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Top
-                        ) {
-                            LazyColumnDemo()
-                        }
-                    }
-                    
+                    MainScreen()
                 }
             }
         }
+    }
+}
+
+//pre questionnaire components
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        MainHeader()
+        DisplayOne()
+        EnterDate()
     }
 }
 
